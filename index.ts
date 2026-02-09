bea54d9d3c5ea461de16a2e3fca4c25785f7071d
Himesh Dua <himeshdua22@gmail.com>
2026-02-09 17:22:29 +0500

    fibonacciSequenceGenerator

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import {deflateSync} from 'bun';
+
 const isPalindrome = (word: string) => {
   if (!word.length) return;
   if (typeof word !== 'string') return;
@@ -9,37 +11,33 @@ const isPalindrome = (word: string) => {
 
 // console.log(isPalindrome('Kayak'));
 
-function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  if (!nums1.length || !m || !nums2.length || !n) return;
+// function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+//   if (!nums1.length || !m || !nums2.length || !n) return;
 
-  let i = m - 1;        // last real element in nums1
-  let j = n - 1;        // last element in nums2
-  let k = m + n - 1;    // last position in nums1
+//   let i = m - 1;        // last real element in nums1
+//   let j = n - 1;        // last element in nums2
+//   let k = m + n - 1;    // last position in nums1
 
+//   while (j >= 0) {
+//     if (i >= 0 && (nums1[i]!) > (nums2[j]!)) {
+//       nums1[k] = nums1[i];
+//       k--;
+//       i--;
+//     } else {
+//       nums1[k] = nums2[j];
+//       k--;
+//       j--;
+//     }
+//   }
 
-  while (j >= 0) {
-    if (i >= 0 && (nums1[i]!) > (nums2[j]!)) {
-      nums1[k] = nums1[i];
-      k--;
-      i--;
-    } else {
-      nums1[k] = nums2[j];
-      k--;
-      j--;
-    }
-  }
-
-
-
-  //   nums1.splice(m + n);
-  // nums2.splice(n);
-  // nums1.push(...nums2);
-  // nums1.sort((accu, b) => a - b);
-  // nums1 = nums1.filter(Boolean);
-  // console.log(nums1);
-}
+//   //   nums1.splice(m + n);
+//   // nums2.splice(n);
+//   // nums1.push(...nums2);
+//   // nums1.sort((accu, b) => a - b);
+//   // nums1 = nums1.filter(Boolean);
+//   // console.log(nums1);
+// }
 // console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
-
 
 async function sleep(millis: number): Promise<void> {
   await new Promise(resolve => setTimeout(resolve, millis));
@@ -56,21 +54,19 @@ async function sleep(millis: number): Promise<void> {
 //   return this.length ? this[this.length - 1] : -1;
 // };
 
-
 // const arr = [];
 // console.log('arr.last(): ', arr.last());
-
 
 function isPalindromeInt(x: number) {
   const reversedInt = x.toString().split('').reverse().join('');
   return x.toString() === reversedInt;
-};
+}
 
 // console.log(isPalindromeInt(112));
 
 function romanToInt(s = 'LVIII') {
-  const romanvals = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 };
-  type RomanKey = keyof typeof romanvals
+  const romanvals = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+  type RomanKey = keyof typeof romanvals;
 
   const pairs = s.split('') as RomanKey[];
   let total = 0;
@@ -80,41 +76,58 @@ function romanToInt(s = 'LVIII') {
     const next = romanvals[pairs[i + 1]!];
 
     if (next && current < next) {
-      total -= current
+      total -= current;
     } else {
-      total += current
+      total += current;
     }
-
   }
-  console.log("Input:", s, "Result:", total);
-};
-romanToInt("III")
-romanToInt("LVIII")
-romanToInt("MCMXCIV")
-
+  console.log('Input:', s, 'Result:', total);
+}
+// romanToInt("III")
+// romanToInt("LVIII")
+// romanToInt("MCMXCIV")
 
 type Counter = {
-  increment: () => number,
-  decrement: () => number,
-  reset: () => number,
-}
+  increment: () => number;
+  decrement: () => number;
+  reset: () => number;
+};
 
 function createCounter(init: number): Counter {
   let total = init;
   return {
     increment: () => ++total,
     decrement: () => --total,
-    reset: () => total = init
+    reset: () => (total = init),
+  };
+}
+
+// const counter = createCounter(5);
+// console.log('increment:', counter.increment());
+// console.log('increment:', counter.increment());
+// console.log('reset:', counter.reset());
+// console.log('decrement:', counter.decrement()); // 4
+// console.log('decrement:', counter.decrement()); //3
+// console.log('decrement:', counter.decrement()); //2
+
+function* fibonacciSequenceGenerator() {
+  let a = 0;
+  let b = 1;
+
+  yield a;
+  yield b;
+
+  while (true) {
+    let temp = b;
+    b = a + b;
+    yield b;
+    a = temp;
   }
-};
+}
 
-
-const counter = createCounter(5);
-console.log('increment:', counter.increment());
-console.log('increment:', counter.increment());
-console.log('reset:', counter.reset());
-console.log('decrement:', counter.decrement()); // 4
-console.log('decrement:', counter.decrement()); //3
-console.log('decrement:', counter.decrement()); //2
-
-
+const gen = fibonacciSequenceGenerator();
+console.log(
+  Array.from({length: 21})
+    .map(_ => gen.next().value)
+    .join('')
+);
